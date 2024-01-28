d66146c7b8644913baafc77e29175af353d063df
victorseara <victorseara.me@gmail.com>
2024-01-28 18:11:37 -0300

    change

--- apps/web/pages/api/[...api].ts
+++ apps/web/pages/api/[...api].ts
@@ -4,13 +4,19 @@ import {
   GetHomeDataRoute,
   GetHomeDataService,
 } from '@self/home-page-final/server';
+import {
+  GetLaunchpadConfig,
+  GetLaunchpadRoute,
+  GetLaunchpadService,
+} from '@self/pages/launchpad/server';
 
 const apiRouter = new Router({
   log: 'info',
   routes: {
     [GetHomeDataConfig.injectionKey]: GetHomeDataRoute,
+    [GetLaunchpadConfig.injectionKey]: GetLaunchpadRoute,
   },
-  services: [GetHomeDataService],
+  services: [GetHomeDataService, GetLaunchpadService],
 });
 
 export default apiRouter.handler;
